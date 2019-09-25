9bc5190631d0a76d4557f8c1904b3e50878843e7
maztertwo <59010555@kmitl.ac.th>
2019-09-25 19:43:05 +0700

    api register can be use

--- route/product.js
+++ route/product.js
@@ -6,6 +6,19 @@ const fs = require('fs');
 temp=[];
 temp = {name:'two'};
 
+function send(dataTemp){
+    mongo.connect('mongodb://localhost:27017', function (err, client) {
+        if (err) throw err;
+        var db = client.db('aciids2020');
+      
+        db.collection('aciids2020').insert(dataTemp, function (err, result) {
+          if (err) throw err;
+          console.log("Insert successful");
+          client.close();
+        });
+      });
+}
+
 router.get('/',(req,res,next)=> {
 
     res.status(200).json({
@@ -27,6 +40,39 @@ router.get('/test', function(req , res){
         res.end();
     });
 });
+
+router.get('/signup', function(req , res){
+    res.writeHead(200,{'Content-Type':'text/html'});
+    fs.readFile('./CODE/html/SignupPage.html',null, function(err,data){
+        if(err){
+            res.writeHead(404);
+            res.write("File not found");
+        }
+        else{
+            res.write(data);
+        }
+        res.end();
+    });
+});
+
+
+router.post('/signupConfirm', function(req , res){
+            var name = req.body.name;
+            var username = req.body.username;
+            var password = req.body.password;
+            var confirmPass = req.body.confirmPass;
+            dataTemp = {name,username,password}
+            if(password == confirmPass){
+               send(dataTemp);
+               res.send("Mission Complete")
+            }
+            else{
+                console.log(name+"Password Not Match")
+                res.redirect('/signup')
+            }
+        
+        res.end();
+    });
 
 router.post('/insert',(req,res,next)=>{   // Test insert Data from zabbix to Database
     mongo.connect('mongodb://localhost:27017', function (err, client) {
--- server.js
+++ server.js
@@ -1,32 +1,10 @@
-var app = require('express')();
+var app = require('./app');
 var http = require('http');
-var bodyParser = require('body-parser');
-var fs = require('fs');
+
 var port = process.env.PORT || 8080
+var server = http.createServer(app);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 
-app.get('/', function(req , res){
-    res.writeHead(200,{'Content-Type':'text/html'});
-    fs.readFile('./CODE/html/LoginPage.html',null, function(err,data){
-        if(err){
-            res.writeHead(404);
-            res.write("File not found");
-        }
-        else{
-            res.write(data);
-        }
-        res.end();
-    });
-});
+console.log("Website use url locahost:"+port);
 
-app.get('/index', function(req,res){
-    res.send('<h1>This is index page sensei !!</h1>');
-});
-
-app.listen(port, function() {
-    console.log("Website use url locahost:"+port);
-});
+server.listen(port, function() {});
